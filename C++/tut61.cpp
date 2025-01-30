c47c3eb72e066febc88c0c0c7044ee709c131348
Reaven010 <shubhtiwari19419@gmail.com>
2025-01-30 10:01:48 +0530

    file handeling

---
+++ C++/tut61.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+int main(){
+    //for writing in  a file
+
+    // string name;
+    // ofstream out("sample60.txt");
+    // cout<<"enter your name: ";
+    // cin>>name;
+    // out<<"my name is " + name;
+    // out.close();//frees space
+
+    //for reading a file
+    ifstream in("sample60.txt");
+    string content;
+    getline(in,content);
+    cout<<"the content of this file is : \n"<<content;
+
+    return 0;
+}
