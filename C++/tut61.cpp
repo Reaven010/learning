647892928b9c273ade4381ad060501eff86d0a6d
Reaven010 <shubhtiwari19419@gmail.com>
2025-01-30 10:04:36 +0530

    file handeling

--- C++/tut61.cpp
+++ C++/tut61.cpp
@@ -5,12 +5,12 @@ using namespace std;
 int main(){
     //for writing in  a file
 
-    // string name;
-    // ofstream out("sample60.txt");
-    // cout<<"enter your name: ";
-    // cin>>name;
-    // out<<"my name is " + name;
-    // out.close();//frees space
+    string name;
+    ofstream out("sample60.txt");
+    cout<<"enter your name: ";
+    cin>>name;
+    out<<"my name is " + name;
+    out.close();//frees space
 
     //for reading a file
     ifstream in("sample60.txt");
