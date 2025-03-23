cfd54755d393a6afdae64c0e9bec1e81d9e3b815
Reaven010 <shubhtiwari19419@gmail.com>
2025-03-23 15:06:20 +0530

    binasry search

---
+++ C++/practice/binary_search.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int binary_search(int arr[],int n ,int key){
+    int start=0,end =n-1,mid;
+    while(start<=end){
+        mid=(start+end)/2 ;//mid point
+        if (arr[mid]==key){
+            return mid;
+        }
+        else if (arr[mid]<key){
+            start=mid+1;
+        }
+        else{
+            end =mid-1;
+        }
+        }
+        return -1;
+    }
+
+int main(){
+    int arr[1000],n,key;
+    cout <<"enter size of array"<<endl;
+    cin>>n;
+    for (int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    cout<<"enter a key";
+    cin>>key;
+    cout<<binary_search(arr,n,key);
+    return 0;
+}
