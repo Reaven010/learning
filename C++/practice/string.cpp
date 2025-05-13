c676a2b56fbef4b4b8bd7a15b736fb6abefe07c7
Reaven010 <shubhtiwari19419@gmail.com>
2025-05-13 18:22:12 +0530

    lecture 37

--- C++/practice/string.cpp
+++ C++/practice/string.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 #include <algorithm>
 #include<climits>
+#include <string>
+#include <vector>
 using namespace std;
+
+void rotate_clockwise(string &s){
+    char temp = s.size()-1;
+    for (int i=s.size()-2;i>=0;i--){
+        s[i]=s[i-1];
+    }
+    s[0]=temp;
+}
+
+void rotate_anticlockwise(string &s){
+    char temp =s[0];
+    for(int i=1;i<s.size();i++){
+        s[i]=s[i+1];
+    }
+    s[s.size()-1]=temp;
+}
 
 int main() {
     // char arr[20];
@@ -46,4 +64,56 @@ int main() {
         
     }
      cout<<"palindrome"<<endl;
-}
+
+    //rotation of string
+    s1="amazon";
+    s2="azonam";
+    if (s1.size()!=s2.size()){return 0;}
+        char temp;
+        
+        //repeate twice
+        for (int count=0;count<2;count++){
+            //rotate clockwise once  
+            temp=s1[s1.size()-1];
+            int index=s1.size()-1;
+            while (index>0){
+                s1[index]=s1[index-1];
+                index--;
+                
+            }
+            s1[0]=temp;
+            cout<<"colckwise "<<s1<<endl;
+        }
+        if (s1==s2){
+            cout<<"clockwise rotation"<<endl;
+        }
+        for (int count =0;count<4;count++){
+            //rotate anticlockwise once
+            temp=s1[0];
+            int index=0;
+            while (index<=s1.size()-1){
+                s1[index]=s1[index+1];
+                index++;
+            }
+            s1[s1.size()-1]=temp;
+            cout<<"anticlockwise "<<s1<<endl;
+        }
+        if (s1==s2){
+            cout<<"anticlockwise rotation"<<endl;
+        }
+
+        //panagram
+        cout<<"panagram"<<endl;
+        string sentence ="thequickbrownfoxjumpsoverthelazydog";
+        vector<int> lower(26,0);
+        for (int i=0;i<sentence.size();i++){
+            lower[sentence[i]-'a']++;
+        }
+        for (int i=0;i<26;i++){
+            cout<<lower[i]<<" ";
+            if (lower[i]==0){
+                return 1;
+            }
+        }
+        return 0;
+    }
