941405b053f70d6127021fb68e599f465ed03456
Reaven010 <shubhtiwari19419@gmail.com>
2025-07-12 15:18:39 +0530

    12-7-2025

---
+++ java/self/area_of_circle.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class area_of_circle{
+    public static void main(String args[]) {
+        double radius;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter radius of circle");
+        radius=sc.nextDouble();
+        System.out.println(3.14*radius*radius);
+    }
+}
