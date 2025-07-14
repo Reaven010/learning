25d293d363238d20276e4336bab5f4db472335b2
Reaven010 <shubhtiwari19419@gmail.com>
2025-07-14 12:30:18 +0530

    14-07-2025

---
+++ C++/practice/gcd.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+#include<climits>
+using namespace std;
+int gcd (int a,int b){
+    if (a>b){
+        swap(a,b);
+    }
+    while (b){
+        int r=a%b;
+        a=b;
+        b=r;
+    }
+    return a;
+}
+int main() {
+    cout<<"gcd is "<<gcd (70,35)<<endl;
+    cout<<"gcd is "<<gcd (90,15)<<endl;
+}
