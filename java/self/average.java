60bf9a53b01d13eec8fcc292ffd0c0f999ccf1af
Reaven010 <shubhtiwari19419@gmail.com>
2025-07-21 12:11:43 +0530

    21-07-2025

---
+++ java/self/average.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class average {
+    public static void main(String args[]){
+        int num1,num2,num3;
+        double average;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter numbers");
+        num1=sc.nextInt();
+        num2=sc.nextInt();
+        num3=sc.nextInt();
+        average=(num1+num2+num3)/3;
+        System.out.println("average is : "+average);
+        sc.close();
+
+    }
+}
