b2e85675c5f31bf83eb5d50d3fc613f2e00de509
Reaven010 <shubhtiwari19419@gmail.com>
2025-08-11 22:24:23 +0530

    11-08-2025

--- java/self/fever.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-public class fever {
-    public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
-        double temp=sc.nextDouble();
-        System.out.println((temp>=100)?"fever":"normal");
-    }
-}
--- java/self/maths.java
+++
@@ -1,5 +0,0 @@
-public class maths {
-    public static void main(String args[]){
-    System.out.println(Math.sqrt(100));
-}
-}
