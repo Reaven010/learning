a06666d4a39e393aed464c65e9188a4588cbf572
Reaven010 <shubhtiwari19419@gmail.com>
2025-11-01 13:03:38 +0530

    1 november

---
+++ web_development/javascript/operator.js
@@ -0,0 +1,10 @@
+// operator 
+// arithmatic operators 
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2%2);
+console.log(5**2);//this is used to indecate power 
+
