640e2c67cf0622d2b3d7d538969fc1a1f8960fdf
Reaven010 <shubhtiwari19419@gmail.com>
2025-11-28 19:53:11 +0530

    28 november

--- C++/practice/stack.cpp
+++ C++/practice/stack.cpp
@@ -1,43 +1,74 @@
 #include <iostream>
+#include <algorithm>
+#include<climits>
 using namespace std;
 
-void push(int arr[],int &top,int n){
-    if (top == n-1){
-        cout<<"stack overflow"<<endl;
-    }
-    else{
-        top++;
-        cout<<"enter element "<<endl;
-        cin >>arr[top];
-    }
-}
-void pop(int arr[],int &top,int n){
-    if(top==-1){
-        cout<<"stack underflow"<<endl;
-    }
-    else{
-        cout<<arr[top]<<endl;
-        arr[top]=0;
-        top--;
-    }
-}
-void display(  int arr[],int top){
-    for (int i=0;i<=top;i++){
-        cout<<arr[i]<<" ";
-    }
-    cout<<endl;
-}
-void peek(int arr[],int &top){
-    cout<<arr[top]<<endl;
-}
-int main() {
-    int arr[5];
-    int top=-1;
-    push(arr,top,5);
-    push(arr,top,5);
-    pop(arr,top,5);
-    push(arr,top,5);
-    pop(arr,top,5);
-    pop(arr,top,5);
+class stack{
+    int *arr;
+    int top;
+    int size;
+    
+    public:
 
+    bool flag;
+
+    stack(int s){
+        arr=new int[s];
+        size=s;
+        top=-1;
+        flag=1;
+    }
+
+    //push
+    void push(int val){
+        if(top==size-1){
+            cout<<"stack overflow"<<endl;
+            return ;
+        }
+        top++;
+        arr[top]=val;
+        flag=0;
+        cout<<"value pushed into stack : "<<val<<endl;
+    }
+    //pop
+    void pop(){
+        if(top==-1){
+            cout<<"stack underflow"<<endl;
+            flag=1;
+            return;
+        }
+        top--;
+        cout<<"element popped : "<<arr[top+1]<<endl;
+    }
+    //peek
+    int peek(){
+        if(top==-1){
+            cout<<"stack is empty"<<endl;
+            return -1;
+        }
+        return arr[top];
+    }
+    //isempty
+    bool isempty(){
+        if(top==-1){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+    //issize
+    int issize(){
+        return top+1;
+    }
+
+};
+
+int main() {
+    stack s(5);
+    s.push(-1);
+    int val=s.peek();
+    if(s.flag==0){
+        cout<<val<<endl;
+    }
 }
